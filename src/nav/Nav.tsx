52670a632c798fcbc32c7b65ef02afa6318975ba
D2 <dsmontecastro1@up.edu.ph>
2023-08-18 14:18:48 +0800

    [NAV] Reorganized Line & Items

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -28,32 +28,20 @@ export default function Nav({ index, pageRefs }: Props) {
 
     return (
 
-        <div id='nav' className={`w-28 h-full mx-[1%] ${Layout.col}`}>
+        <div id='nav' className={`w-28 h-full mr-[1%] ${Layout.col}`}>
 
-            <header id='nav-head' className='h-min my-3 spac flex-initial text-4xl text-left tracking-widest indent-3'
+            <header id='nav-head' className={`h-min my-3 spac flex-initial text-4xl tracking-widest ${Layout.center}`}
             > NAV </header>
 
             <div id='nav-body' className={`my-3 flex-1 ${Layout.row}`}>
 
-                <div id='nav-items' className={`z-10 w-[95%] h-full ${Layout.col}`}>
+                <div id='nav-line' className={`w-3.5 my-2 translate-x-6 ${Colors.bg}`} />
+
+                <div id='nav-items' className={`z-10 w-[95%] h-full ml-0.5 ${Layout.col}`}>
                     {Array(pageCount).fill(null).map((_, i) => makeNavItem(i))}
                 </div>
-
-                {/* <div id='nav-progress' className={`w-[10%] my-2 -ml-1 -translate-x-20 ${Colors.bg}`} /> */}
-                {/* <div id='nav-progress' className={`w-[5%] my-2 -translate-x-24 ${Colors.bg}`} /> */}
-                <div id='nav-progress' className={`w-3 my-2 -ml-4 -translate-x-20 ${Colors.bg}`} />
 
             </div>
-
-            {/* <div id='nav-body' className={`my-3 flex-1 ${Layout.row}`}>
-
-                <div id='nav-items' className={`z-10 h-full ${Layout.col}`}>
-                    {Array(pageCount).fill(null).map((_, i) => makeNavItem(i))}
-                </div>
-
-                <div id='nav-progress' className={`w-3 my-2 -ml-1 -translate-x-20 ${Colors.bg}`} />
-
-            </div> */}
 
         </div>
 
