9bd50b2c9e06c07401dfbbc4e7b496daf69a0b9c
D2 <dsmontecastro1@up.edu.ph>
2023-08-20 14:57:07 +0800

    [PAGES] Finalized -About- Layout

--- src/page/Styles.tsx
+++ src/page/Styles.tsx
@@ -1,6 +1,18 @@
-export default class Layout {
+export class Layout {
     static fill = 'w-full w-full';
-    static center = 'flex justify-center items-center';
     static row = 'flex flex-row';
     static col = 'flex flex-col';
+
+    static center = 'flex justify-center items-center';
+    static rBorder = 'border-r-2 border-spacing-5 border-collapse'
 }
+
+export class Colors {
+    static gradience = 'animate-gradience bg-gradient-to-r from-blue-500 via-purple-500 to-green-500 bg-clip-text text-transparent'
+}
+
+export class Text {
+    static header = 'text-6xl font-black'
+}
+
+
--- src/page/pages/About.tsx
+++ src/page/pages/About.tsx
@@ -1,18 +1,53 @@
-import Layout from "../Styles";
+import { Colors, Layout } from "../Styles";
 
 export default function About() {
 
     const name = 'about';
 
+    function label(id: string) {
+        return `${name}-${id}`;
+    }
+
+    function parseTime() {
+        const hour = new Date().getHours();
+        let time = '???';
+
+        if (hour == 0) time = 'Day';
+        else if (hour < 12) time = 'Morning';
+        else if (hour == 12) time = 'Noon';
+        else if (hour < 18) time = 'Afternoon'
+        else if (hour < 20) time = 'Evening';
+        else if (hour < 24) time = 'Night';
+
+        return time.toUpperCase();
+    }
+
     return (
 
-        <div id={name} className={`w-[95%] h-[95%] bg-red-500 ${Layout.center}`}>
+        <div id={name} className={`w-full h-min m-10 px-10 py-10 bg-slate-800 bg-opacity-50 ${Layout.col}`}>
 
-            <header>
-                Aabb
-            </header>
+            <div id={label('title')} className={`${Layout.col}`}>
 
-        </div>
+                <div id={label('greeting')} className={`mb-4 text-left text-8xl font-black flex-shrink ${Layout.row}`}>
+                    <p> GOOD// </p>
+                    <p className={`${Colors.gradience}`}> {parseTime()}! </p>
+                </div>
+
+                <div id={label('intro')} className={`mb-7 pb-5 text-left text-5xl font-black flex-shrink`}>
+                    I'm Daniel, aspiring Web|App Developer
+                </div>
+
+            </div>
+
+            <div id={label('info')} className={`w-4/5 p-3 text-lg text-justify bg-black bg-opacity-30 flex-1 ${Layout.center}`}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id estlaborum.
+            </div>
+
+        </div >
 
     );
 
