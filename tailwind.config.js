931effa2d70030860bee000f4768c16f219a622a
D2 <dsmontecastro1@up.edu.ph>
2023-08-24 17:46:13 +0800

    [TCSS] Added Screen & Wheel Definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require( 'tailwindcss/defaultTheme' );
+
 export default {
   darkMode: true,
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
+
+  plugins: [],
+
   theme: {
+
+    screens: {
+      '3xs': '300px',
+      '2xs': '360px',
+      'xs': '400px',
+      ...defaultTheme.screens,
+    },
+
     extend: {
 
+      scale: {
+        '25': '0.25'
+      },
+
       animation: {
-        gradience: 'slide 5s ease infinite',
+        'cw': 'cw 10s linear infinite',
+        'ccw': 'ccw 10s linear infinite',
+        'gradience': 'slide 5s ease infinite'
       },
 
       keyframes: {
-        slide: {
+
+        'cw': {
+          '0%': { 'transform': 'rotate(0deg)' },
+          '100%': { 'transform': 'rotate(360deg)' }
+        },
+
+        'ccw': {
+          '0%': { 'transform': 'rotate(0deg)' },
+          '100%': { 'transform': 'rotate(-360deg)' }
+        },
+
+        'slide': {
           '0%, 100%': {
             'background-size': '200% 200%',
             'background-position': 'left center',
@@ -24,9 +55,9 @@ export default {
             'background-position': 'right center',
           },
         },
+
       },
 
     },
   },
-  plugins: [],
 }
